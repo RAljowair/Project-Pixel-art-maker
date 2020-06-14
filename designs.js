c950cb9a01fbe5fe2d09fc6b3d8017a299730446
RAljowair <rayanaljowair@gmail.com>
2020-06-14 08:26:40 +0300

    Added java script code

--- designs.js
+++ designs.js
@@ -1,10 +1,23 @@
 // Select color input
-// Select size input
+const slectedColor = document.getElementById("colorPicker").innerHTML;
 
+// Select size input
+const gridHeight = document.getElementById("inputHeight").vlaue;
+const gridWidth = document.getElementById("inputWidth").vlaue;
+const table = document.getElementById('pixelCanvas');
 // When size is submitted by the user, call makeGrid()
+makeGrid();
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
+    const cell = document.createElement('td');
 
+    for (var row = 1; row <= gridHeight; row++) {
+        const tableRow = document.createElement('tr');
+        table.appendChild(tableRow);
+        for (var column = 1; column <= gridWidth; column++) {
+            tableRow.appendChild(cell);
+        }
+    }
 }
