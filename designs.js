a85b17455d3bc4c4c464f43f524b90c030803c4f
RAljowair <rayanaljowair@gmail.com>
2020-06-16 04:22:37 +0300

    1- Fixed a problem in the grid sizing 2- Added coloring by clicking the cell

--- designs.js
+++ designs.js
@@ -1,17 +1,26 @@
 // Select color input
-const slectedColor = document.getElementById("colorPicker").innerHTML;
-
+const colorPicker = document.getElementById("colorPicker");
 // Select size input
 const inputHeight = document.getElementById("inputHeight");
 const inputWidth = document.getElementById("inputWidth");
 const pixelCanvas = document.getElementById('pixelCanvas');
-// When size is submitted by the user, call makeGrid()
 const sizePicker = document.getElementById('sizePicker');
 
+
+// When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener('submit', function (e){
     e.preventDefault();
-    makeGrid();    
+    
+    const resetRows = document.querySelectorAll('tr');
+    
+    for(var i = 0 ; i < resetRows.length ; i++){
+        resetRows[i].remove();
+    }
+    
+    makeGrid();
 })
+
+// Convert color value from hex to rgb.
 
 
 function makeGrid() {
@@ -26,4 +35,27 @@ function makeGrid() {
             allRows[i].appendChild(document.createElement('td'));
         }
     }
+
+    var allCells = document.querySelectorAll('td');
+
+    for(var i = 0; i < allRows.length ; i++){
+        allRows[i].setAttribute('id', 'row-' + i);
+    }
+
+    for(var i = 0; i < allCells.length ; i++){
+        allCells[i].setAttribute('id', 'cell-' + i);
+        allCells[i].addEventListener('click', function (e){
+            e.preventDefault;
+
+            
+            if(this.style.backgroundColor == 'rgb(255, 255, 255)'){
+                this.style.backgroundColor = colorPicker.value;
+            }else if(this.style.backgroundColor == colorPicker.value){
+                this.style.backgroundColor = 'rgb(255, 255, 255)';
+            }
+        })
+    }
+    
+    document.getElementById('sizePicker').reset();
 }
+
