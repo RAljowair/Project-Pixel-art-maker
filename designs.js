31b47816a889457697eefd7f3e7f1b0eb5a8cd25
RAljowair <rayanaljowair@gmail.com>
2020-06-17 22:46:31 +0300

    The final version style edited also added deleting one cell's color ability

--- designs.js
+++ designs.js
@@ -1,35 +1,33 @@
 // Select color input
-const colorPicker = document.getElementById("colorPicker");
+const COLOR_PICKER = document.getElementById("colorPicker");
 // Select size input
-const inputHeight = document.getElementById("inputHeight");
-const inputWidth = document.getElementById("inputWidth");
-const pixelCanvas = document.getElementById('pixelCanvas');
-const sizePicker = document.getElementById('sizePicker');
+const INPUT_HEIGHT = document.getElementById("inputHeight");
+const INPUT_WIDTH = document.getElementById("inputWidth");
+const PIXEL_CANVAS = document.getElementById('pixelCanvas');
+const SIZE_PICKER = document.getElementById('sizePicker');
 
 
 // When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener('submit', function (e){
+SIZE_PICKER.addEventListener('submit', function(e) {
     e.preventDefault();
-    
-    const resetRows = document.querySelectorAll('tr');
-    
+    var resetRows = document.querySelectorAll('tr');
+    // for loop to clear the grid.  
     for(var i = 0 ; i < resetRows.length ; i++){
         resetRows[i].remove();
     }
-    
     makeGrid();
 })
 
-// Convert color value from hex to rgb.
 
-
+// MakeGrid() method will create the entire grid and will intilaiz the rows and cells.
 function makeGrid() {
     
-    rows = inputHeight.value;
-    columns = inputWidth.value;
+    rows = INPUT_HEIGHT.value;
+    columns = INPUT_WIDTH.value;
+
 
     for(var i = 0; i < rows ; i++){
-        pixelCanvas.appendChild(document.createElement('tr'));
+        PIXEL_CANVAS.appendChild(document.createElement('tr'));
         for(var j = 0; j < columns ; j++){
             var allRows = document.querySelectorAll('tr');
             allRows[i].appendChild(document.createElement('td'));
@@ -42,20 +40,19 @@ function makeGrid() {
         allRows[i].setAttribute('id', 'row-' + i);
     }
 
-    for(var i = 0; i < allCells.length ; i++){
-        allCells[i].setAttribute('id', 'cell-' + i);
-        allCells[i].addEventListener('click', function (e){
-            e.preventDefault;
 
-            
-            if(this.style.backgroundColor == 'rgb(255, 255, 255)'){
-                this.style.backgroundColor = colorPicker.value;
-            }else if(this.style.backgroundColor == colorPicker.value){
-                this.style.backgroundColor = 'rgb(255, 255, 255)';
-            }
+    for(var i = 0; i < allCells.length ; i++){
+        
+        allCells[i].setAttribute('id', 'cell-' + i);
+
+        allCells[i].addEventListener('click', function(e) {
+            e.preventDefault;
+            // The First click will fill the cell with the chosen color, The second click will clear it from the color.
+            if(!(this.style.backgroundColor)){
+                this.style.backgroundColor = COLOR_PICKER.value;
+            }else {
+                this.style.backgroundColor = '';
+            }            
         })
     }
-    
-    document.getElementById('sizePicker').reset();
 }
-
